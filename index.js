fe50fcf05640f8d2010b8e445dae3126fc4db740
Dhruvil Lakhtaria <lakhtariadhruvil123@gmail.com>
2022-02-16 03:43:29 +0530

    3-D effect added

---
+++ index.js
@@ -0,0 +1,17 @@
+tags = document.getElementsByClassName("tags");
+
+for (el of tags) {
+  el.setAttribute("data-atropos-offset", "2%");
+}
+
+card_images = document.getElementsByClassName("card__image");
+
+for (el of card_images) {
+  el.setAttribute("data-atropos-offset", "5%");
+}
+
+buttons = document.getElementsByClassName("custom-btn");
+
+for (el of buttons) {
+    el.setAttribute("data-atropos-offset", "10%");
+  }
