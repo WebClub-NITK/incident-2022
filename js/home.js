216c76200bd5cff849c9e70e3abf6f01623f5127
Sriram2001 <sriramudupi19@gmail.com>
2022-02-19 19:12:47 +0530

    Added navbar content

--- js/home.js
+++ js/home.js
@@ -1,2 +1,9 @@
 var scene = document.getElementById('landing-view-scene');
 var parallaxInstance = new Parallax(scene)
+
+// const navbarToggleButton = document.getElementById('toggle-navbar');
+// console.log(navbarToggleButton)
+function navbarToggleFun() {
+    console.log('togglne')
+    document.getElementById('navbar').classList.toggle('open');
+}
