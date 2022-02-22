dd813f1728ef256e9ec753d0519233fffc603591
Sriram2001 <sriramudupi19@gmail.com>
2022-02-22 19:04:59 +0530

    Card styles

--- js/events.js
+++ js/events.js
@@ -2,25 +2,11 @@ const all_events=[
     {
         "title": "Raaga Rhapsody",
         "days": ["1"],
+        "tagline": "Pure, raw, strong and moving",
+        "desc": "Pure, raw, strong and moving. INCIDENT 2022 Musicals brings to you, Raaga-Rhapsody, the solo vocals competition. From Ballads to Alaaps, this event gives you a stage to wow us with your singing and a chance to win the title of “Voice of Inci”.",
+        "location": "SAC (Students Activity Center)",
+        "date-time": "4th March 2022, 1:30 PM Onwards",
         "image": "assets/img/cover4.jpg",
         "type": "Music_Dance"
-    },
-    {
-        "title": "Pulse",
-        "days": ["1"],
-        "image": "assets/img/cover4.jpg",
-        "type": "Music_Dance"
-    },
-    {
-        "title": "Lit1",
-        "days": ["2"],
-        "image": "assets/img/cover4.jpg",
-        "type": "Literary"
-    },
-    {
-        "title": "Lit2",
-        "days": ["1"],
-        "image": "assets/img/cover4.jpg",
-        "type": "Literary"
     }
 ]
