3932c1cc1951a7f957318e3a0cb2154651dbdb50
Sriram2001 <sriramudupi19@gmail.com>
2022-02-22 14:32:34 +0530

    Add day and section filters

---
+++ js/events.js
@@ -0,0 +1,26 @@
+const all_events=[
+    {
+        "title": "Raaga Rhapsody",
+        "days": ["1"],
+        "image": "assets/img/cover4.jpg",
+        "type": "Music_Dance"
+    },
+    {
+        "title": "Pulse",
+        "days": ["1"],
+        "image": "assets/img/cover4.jpg",
+        "type": "Music_Dance"
+    },
+    {
+        "title": "Lit1",
+        "days": ["2"],
+        "image": "assets/img/cover4.jpg",
+        "type": "Literary"
+    },
+    {
+        "title": "Lit2",
+        "days": ["1"],
+        "image": "assets/img/cover4.jpg",
+        "type": "Literary"
+    }
+]
