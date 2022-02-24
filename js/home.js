6577705009c8591d9bff3daf9cf4b4ae0e9a903e
Sriram2001 <sriramudupi19@gmail.com>
2022-02-24 13:28:55 +0530

    Add meta tags, optimise images

--- js/home.js
+++ js/home.js
@@ -1,8 +1,10 @@
 var scene = document.getElementById('landing-view-scene');
-var parallaxInstance = new Parallax(scene)
+var parallaxInstance = new Parallax(scene);
 
 var scene_1 = document.getElementById('theme-image');
-var parallaxInstance_1 = new Parallax(scene_1)
+var parallaxInstance_1 = new Parallax(scene_1);
+
+ScrollOut();
 
 function navbarToggleFun() {
     document.getElementById('navbar').classList.toggle('open');
