a41888cb14f49f6b072e51ba4fcb8904a518e353
Parth Mittal <mittalparth22@gmail.com>
2022-11-28 15:57:25 +0530

    update sponsors for 42nd edition

--- js/sponsors.js
+++ js/sponsors.js
@@ -1,144 +1,74 @@
 var sponsors = [
     {
-        "name": "Karenge",
-        "title": "Title Sponsors",
-        "img": "assets/sponsors/Karenge.png"
-    },
-    {
-        "name": "The Food Mall",
-        "title": "Title Sponsor for Hogathon",
-        "img": "assets/sponsors/food mall.png"
-    },
-    {
-        "name": "TribeVibe",
-        "title": "Co-Sponsors",
-        "img": "assets/sponsors/TRIBEVIBE.png"
-    },
-    {
-        "name": "Interview Ready",
-        "title": "Co-Sponsors for Inci Talks",
-        "img": "assets/sponsors/Interview ready.png"
-    },
-    {
-        "name": "Ace Manufacturing Systems Ltd.",
-        "title": "Diamond Sponsors",
-        "img": "assets/sponsors/ace manufacturing.png"
-    },
-    {
-        "name": "Elearn Markets",
-        "title": "Financial Education Partners",
-        "img": "assets/sponsors/elearn markets.jpg"
+        "name": "Acer",
+        "img": "assets/sponsors/acer.png"
     },
     {
         "name": "Dare 2 Compete",
-        "title": "Hosting Partners",
         "img": "assets/sponsors/dare2compete.png"
     },
     {
-        "name": "Acer",
-        "title": "Technology Partners",
-        "img": "assets/sponsors/Acer-logo.jpg"
+        "name": "Interview Ready",
+        "img": "assets/sponsors/interview ready logo.jfif"
     },
     {
+        "name": "Elearn Markets",
+        "img": "assets/sponsors/elearnmarkets logo.png"
+    },
+
+    {
         "name": "Monster Energy",
-        "title": "Energy Partners",
-        "img": "assets/sponsors/Monster_Energy_logo.png"
+        "img": "assets/sponsors/monster energy.png"
     },
     {
         "name": "Peps",
-        "title": "Gifting Partners",
-        "img": "assets/sponsors/peps logo.jpg"
+        "img": "assets/sponsors/peps logo.png"
     },
     {
         "name": "Sirona Hygiene",
-        "title": "Hygiene Partners",
         "img": "assets/sponsors/sirona.png"
     },
     {
         "name": "GrabOn",
-        "title": "Savings Partners",
-        "img": "assets/sponsors/grabon.jpg",
+        "img": "assets/sponsors/grabon.png",
         "url": "https://www.grabon.in/"
     },
     {
         "name": "Wonderla",
-        "title": "Amusement Partners",
         "img": "assets/sponsors/wonderla.png"
     },
     {
-        "name": "Memsis",
-        "title": "Mobility Partners",
-        "img": "assets/sponsors/memsis.png"
-    },
-    {
         "name": "KMF Nandini",
-        "title": "Refreshment Partners",
         "img": "assets/sponsors/nandini.png"
     },
     {
-        "name": "MRPL",
-        "title": "Industrial Partners",
-        "img": "assets/sponsors/mrpl.png"
-    },
-    {
         "name": "Lassi Tales",
-        "title": "Lassi Partners",
-        "img": "assets/sponsors/Lassi tales.png"
-    },
-    {
-        "name": "Changepay",
-        "title": "Delivery Partners",
-        "img": "assets/sponsors/changepay.png"
-    },
-    {
-        "name": "School Book Company",
-        "title": "Stationery Partners",
-        "img": "assets/sponsors/sbc.png"
+        "img": "assets/sponsors/lassi tales.jpg"
     },
     {
         "name": "Kreditbee",
-        "title": "Credit Partners",
-        "img": "assets/sponsors/kredit bee.png",
+        "img": "assets/sponsors/kreditbe.png",
         "url": "https://www.kreditbee.in/"
     },
     {
-        "name": "The Cine Galaxy",
-        "title": "Multiplex Partners",
-        "img": "assets/sponsors/cine galaxy.png"
+        "name": "Mangalore Meri Jaan",
+        "img": "assets/sponsors/mangalore meri ja.png"
     },
     {
-        "name": "Big Mishra Peda",
-        "title": "Savoury Partners",
-        "img": "assets/sponsors/big mishra.png"
+        "name": "OJAS  Entertainment",
+        "img": "assets/sponsors/ojas.jpg"
     },
     {
-        "name": "Canara Bank",
-        "title": "Finance Partners",
-        "img": "assets/sponsors/canara bank.png"
+        "name": "Srinivas Verda Saffron",
+        "img": "assets/sponsors/srinivas verda saffron.png"
+    },
+
+    {
+        "name": "ONGC",
+        "img": "assets/sponsors/ongc.png"
     },
     {
-        "name": "Dairy Day",
-        "title": "Ice Cream Partners",
-        "img": "assets/sponsors/dairy day.png"
+        "name": "AMS",
+        "img": "assets/sponsors/ams.jpg"
     },
-    {
-        "name": "Shri Krishna Sanitary",
-        "title": "Sanitation Partners",
-        "img": "assets/sponsors/shri krishna sanitation.png"
-    },
-    {
-        "name": "Yes Bank",
-        "title": "Banking Partners",
-        "img": "assets/sponsors/yes bank.png"
-    },
-    {
-        "name": "Sound Wave Studio",
-        "title": "Studio Partners",
-        "img": "assets/sponsors/soundwave.png"
-    },
-    {
-        "name": "Arrow Labels and Solutions",
-        "title": "Label Partners",
-        "img": "assets/sponsors/arrow.png"
-    }
 ]
